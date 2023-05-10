84e24803967620e83e120b29388d2e3cca966b7d
sandeshghadage <sandeshghadage3@gmail.com>
2023-05-10 16:10:10 +0530

    readme added

--- src/component/button/addListButton/AddListButton.jsx
+++ src/component/button/addListButton/AddListButton.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import style from "./AddListButton.module.css";
 import AddIcon from "@mui/icons-material/Add";
 
-export default function AddListButton() {
+export default function AddListButton({ onClick }) {
   return (
     <>
-      <button className={style.glassButton}>
+      <button className={style.glassButton} onClick={onClick}>
         <AddIcon />
         Add a List
       </button>
