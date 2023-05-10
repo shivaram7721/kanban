18c940722ebcc3902762bbb4ba1b98779687c2b4
Shubham-Sharma <viku.vsk@gmail.com>
2023-05-10 12:45:50 +0530

    folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 function App() {
 
-
+console.log(DragDropContext)
+console.log(Droppable)
   return (
     <>
+
       <h1>Kanban board</h1>
     </>
   )
