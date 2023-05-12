79cec0db0cc5aed6790349690094d50705e9f278
sandeshghadage <sandeshghadage3@gmail.com>
2023-05-12 18:49:56 +0530

    Card add part complited

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,57 @@
-
-import './App.css'
+import "./App.css";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
-import Nav from './nav/Nav';
+// import Nav from "./component/nav/Nav";
 
 import { useState, useRef } from "react";
 import "./App.css";
 import AddListButton from "./component/button/addListButton/AddListButton";
-import List from "./component/list/list";
 import { Card } from "./component/card/Card";
 import Description from "./component/description/Description";
-
+import { dashboard } from "./data";
+import { CardInput } from "./component/cardInput/CardInput";
+import TitleInput from "./component/titleInput/TitleInput";
+import { dashBoardData } from "./atom/Atom";
+import { useRecoilState } from "recoil";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [open, setOpen] = useState(false);
+  const [listData, setListData] = useRecoilState(dashBoardData);
+  const [listName, setListName] = useState("");
+
+  function handleClick() {
+    setOpen(true);
+  }
+
+  function handleCreateList() {
+    const newListItem = {
+      id: uuidv4(),
+      title: listName,
+      cardList: [],
+      createdAt: Date.now(),
+      activity: [],
+    };
+    // console.log(newListItem);
+    const tempData = [...listData, newListItem];
+    setListData([...tempData]);
+  }
 
   return (
-    <>
-
-
- 
-
-      <AddListButton onClick={() => setOpen(true)} />
-      <Card />
+    <div style={{ display: "flex", gap: "2rem" }}>
+      {/* <Card /> */}
+      {listData.map((ele) => (
+        <Card title={ele.title} />
+      ))}
+      {open ? (
+        <TitleInput
+          onChange={(e) => setListName(e.target.value)}
+          onClick={handleCreateList}
+        />
+      ) : (
+        <AddListButton onClick={handleClick} />
+      )}
       <Description />
-
-    </>
+    </div>
   );
 }
 
--- src/component/card/Card.jsx
+++ src/component/card/Card.jsx
@@ -9,77 +9,77 @@ import { CardInput } from "../cardInput/CardInput";
 import { CardList } from "../cardList/CardList";
 import { useRecoilState } from "recoil";
 import { addCards } from "../../atom/Atom";
-import { DragDropContext} from 'react-beautiful-dnd';
+import { DragDropContext } from "react-beautiful-dnd";
 
+export function Card({ title }) {
+  // const [input, setInput] = useState("add Title");
+  // const [input, setInput] = useState('add Title');
+  const [show, setShow] = useState(true);
 
-export function Card() {
-  const [input, setInput] = useState("add Title");
-    // const [input, setInput] = useState('add Title');
-    const [show, setShow] = useState(true);
+  const [cards, setcards] = useRecoilState(addCards);
 
-    const [cards, setcards] = useRecoilState(addCards)
+  function handleTitle(e) {
+    setInput(e.target.value);
+  }
+  // console.log(input)
 
-    function handleTitle(e) {
-        setInput(e.target.value);
-    }
-    console.log(input)
+  function handleAdd() {
+    setShow(!show);
+  }
 
-    function handleAdd() {
-      setShow(!show)
-    }
+  function handleDrag(result) {
+    console.log(result);
 
-    function handleDrag(result) {
-      console.log(result);
+    const { source, destination } = result;
 
-      const {source, destination} = result;
-
-      if(!destination){
-        return;
-      }
-
-      if(source.destinationId === destination.destinationId && source.index === destination.index) {
-        return;
-      }
-
-      const newCards = Array.from(cards);
-      const [reOrderedCards] = newCards.splice(result.source.index, 1);
-      newCards.splice(result.destination.index, 0, reOrderedCards);
-
-      setcards(newCards);
+    if (!destination) {
+      return;
     }
 
+    if (
+      source.destinationId === destination.destinationId &&
+      source.index === destination.index
+    ) {
+      return;
+    }
+
+    const newCards = Array.from(cards);
+    const [reOrderedCards] = newCards.splice(result.source.index, 1);
+    newCards.splice(result.destination.index, 0, reOrderedCards);
+
+    setcards(newCards);
+  }
 
   return (
     <div className={styles.cardContainer}>
       <DragDropContext onDragEnd={handleDrag}>
-      <div className={styles.titleContainer}>
-        <p className={styles.cardTitle} onChange={handleTitle}>
-          {input}
-        </p>
-        <HiOutlineDotsHorizontal className={styles.dotsIcon} />
-      </div>
-      
-      <div>
-        <CardList />
-      </div>
+        <div className={styles.titleContainer}>
+          <p className={styles.cardTitle} onChange={handleTitle}>
+            {title}
+          </p>
+          <HiOutlineDotsHorizontal className={styles.dotsIcon} />
+        </div>
 
+        <div>
+          <CardList />
+        </div>
       </DragDropContext>
 
-      {
-        show ? (
-          <div className={styles.addCardBtn}>
-        <div onClick={handleAdd} className={styles.cardBtn}>
-          <IoMdAdd className={styles.addIcon} />
-          <p className={styles.addBtn}>Add a card</p>
-        </div>
-        <Tooltip title="create from template">
-          <div className={styles.templateBtn}>
-            <TbTemplate />
+      {show ? (
+        <div className={styles.addCardBtn}>
+          <div onClick={handleAdd} className={styles.cardBtn}>
+            <IoMdAdd className={styles.addIcon} />
+            <p className={styles.addBtn}>Add a card</p>
           </div>
-        </Tooltip>
-      </div>
-        ) : <CardInput show={handleAdd}/>
-      }
+          <Tooltip title="create from template">
+            <div className={styles.templateBtn}>
+              <TbTemplate />
+            </div>
+          </Tooltip>
+        </div>
+      ) : (
+        <CardInput show={handleAdd} />
+      )}
 
       {/* <CardInput /> */}
     </div>
